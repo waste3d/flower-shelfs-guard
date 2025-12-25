d1935def8d17a725418a8c3e2e5ffce161d78425
waste3d <kolya@waste3d.su>
2025-12-25 15:58:56 +0300

    feat: config

---
+++ src/config.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Tuple, List, Dict
+
+@dataclass
+class ZoneConfig:
+    name: str
+    box: Tuple[int, int, int, int] # (x1, y1, x2, y2)
+
+IMAGE_ZONES: Dict[str, List[ZoneConfig]] = {
+    'shelf_full_1.jpg': [
+        ZoneConfig(name="full_zone", box=(0, 0, 183, 275)),
+    ],
+    'shelf_full_2.jpg': [
+        ZoneConfig(name="full_zone", box=(0, 0, 200, 253)),
+    ],
+    'shelf_empty_1.jpg': [
+        ZoneConfig(name="empty_zone", box=(0, 0, 275, 183)),
+    ],
+    'shelf_empty_2.jpg': [
+        ZoneConfig(name="empty_zone", box=(0, 0, 740, 493)),
+    ],
+}
